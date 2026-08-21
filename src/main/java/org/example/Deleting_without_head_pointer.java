f08fffc33006a978f9a9abe6bdab51b73d68c01d
Sahid Reza <sahidreza976@gmail.com>
2026-08-21 19:33:30 +0530

    Add the node class

--- src/main/java/org/example/Deleting_without_head_pointer.java
+++ src/main/java/org/example/Deleting_without_head_pointer.java
@@ -1,7 +1,18 @@
 package org.example;
 
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
 public class Deleting_without_head_pointer {
     public static void main(String[] args) {
+
 
     }
 }
