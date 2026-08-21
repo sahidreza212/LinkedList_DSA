8660f9da07610e1c0a3bb76003dbf88fff5e06ca
Sahid Reza <sahidreza976@gmail.com>
2026-08-21 19:46:37 +0530

    Add the funal commit

--- src/main/java/org/example/Deleting_without_head_pointer.java
+++ src/main/java/org/example/Deleting_without_head_pointer.java
@@ -20,17 +20,17 @@ public class Deleting_without_head_pointer {
 
     public static void printList(Node head){
        while (head != null){
+
           System.out.print(head.data);
+
           if(head.next != null){
-              System.out.println(" -> ");
-              head = head.next;
+              System.out.print(" -> ");
           }
-
+           head = head.next;
        }
-
         System.out.println();
-
     }
+
     public static void main(String[] args) {
 
         Node head = new Node(10);
