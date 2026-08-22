0acdead402d2255ebe86f00e08e45d75b01e3580
Sahid Reza <sahidreza976@gmail.com>
2026-08-22 07:10:02 +0530

    Add the full function of the sortList

--- src/main/java/org/example/Sort_LinkedList.java
+++ src/main/java/org/example/Sort_LinkedList.java
@@ -4,6 +4,28 @@ public class Sort_LinkedList {
 
     public static void sortList(Node head){
 
+        int []count = {0,0,0};
+        Node ptr = head;
+
+        // count the frequency of each element
+        while(ptr != null){
+            count[ptr.data] += 1;
+            ptr = ptr.next;
+        }
+
+        int idx = 0;
+        ptr = head;
+
+        // put the all element in the sort order
+        while( ptr != null){
+            if(count[idx] == 0){
+                idx += 1;
+            }else {
+                ptr.data = idx;
+                count[idx] -= 1;
+                ptr = ptr.next;
+            }
+        }
     }
 
     public static void main(String[] args) {
