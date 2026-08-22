4ec0541cb956856c3dee27d1064e7a8bdc80391d
Sahid Reza <sahidreza976@gmail.com>
2026-08-22 06:22:48 +0530

    Add the full delete duplicate function

--- src/main/java/org/example/Remove_Duplicates.java
+++ src/main/java/org/example/Remove_Duplicates.java
@@ -4,7 +4,32 @@ package org.example;
 public class Remove_Duplicates {
 
     public static Node deleteDuplicates(Node head){
+        if(head == null){
+            return head;
+        }
+        Node prev = head;
+        Node curr = head.next;
 
+        while(curr != null){
+            boolean duplicate = false;
+            Node temp = head;
+
+            while(temp != curr){
+                if(temp.data == curr.data){
+                    duplicate = true;
+                    break;
+                }
+                temp = temp.next;
+            }
+            if(duplicate){
+                prev.next = curr.next;
+                curr = prev.next;
+            }else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        return  head;
     }
     public static void main(String[] args) {
 
