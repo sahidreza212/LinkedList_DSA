b28cf0ae5728694378fc458fda7d7181575cdf46
Sahid Reza <sahidreza976@gmail.com>
2026-08-23 06:43:55 +0530

    Add the first number

--- src/main/java/org/example/Multiply_Two_Linked_List.java
+++ src/main/java/org/example/Multiply_Two_Linked_List.java
@@ -5,6 +5,13 @@ public class Multiply_Two_Linked_List {
 
    public static long multiplyTwoList(Node first,  Node second){
 
+       long num1 = 0, num2 = 0;
+
+       // Travers the first linkedList and construct the first number
+       while(first != null){
+           num1 = num1 * 10 + first.data;
+           first = first.next;
+       }
    }
 
     public static void main(String[] args) {
