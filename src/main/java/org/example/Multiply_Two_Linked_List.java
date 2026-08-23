b8ae5bcab4189cd77b1ebc114cae62ba6c561b6e
Sahid Reza <sahidreza976@gmail.com>
2026-08-23 06:41:25 +0530

    Add the multiply two list function

--- src/main/java/org/example/Multiply_Two_Linked_List.java
+++ src/main/java/org/example/Multiply_Two_Linked_List.java
@@ -3,6 +3,10 @@ package org.example;
 public class Multiply_Two_Linked_List {
    public static final long MOD = 10000000007;
 
+   public static long multiplyTwoList(Node first,  Node second){
+
+   }
+
     public static void main(String[] args) {
 
     }
