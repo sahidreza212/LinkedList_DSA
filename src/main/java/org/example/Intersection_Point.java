69dc8c386bd187cfd2462db45d5622d09c55145f
Sahid Reza <sahidreza976@gmail.com>
2026-08-24 18:46:45 +0530

    Add the full function call

--- src/main/java/org/example/Intersection_Point.java
+++ src/main/java/org/example/Intersection_Point.java
@@ -17,5 +17,27 @@ public class Intersection_Point {
     }
     public static void main(String[] args) {
 
+        // First list: 10 -> 15 -> 30
+        Node head1 = new Node(10);
+        head1.next = new Node(15);
+        head1.next.next = new Node(30);
+
+        // Second list: 3 -> 6 -> 9
+        Node head2 = new Node(3);
+        head2.next = new Node(6);
+        head2.next.next = new Node(9);
+
+        // Create intersection
+        // 3 -> 6 -> 9 -> 15 -> 30
+        head2.next.next.next = head1.next;
+
+        // Find intersection point
+        Node intersectionPoint = intersectionPoint(head1, head2);
+
+        if (intersectionPoint == null) {
+            System.out.println("-1");
+        } else {
+            System.out.println(intersectionPoint.data);
+        }
     }
 }
