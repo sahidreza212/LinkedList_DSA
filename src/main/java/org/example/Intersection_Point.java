def6af80d101c9e52c5d3e8af27227b8bceb3bbf
Sahid Reza <sahidreza976@gmail.com>
2026-08-24 18:38:54 +0530

    Add the intersectionPoint

--- src/main/java/org/example/Intersection_Point.java
+++ src/main/java/org/example/Intersection_Point.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Intersection_Point {
+    public static Node intersectionPoint(Node head1, Node head2){
+
+    }
     public static void main(String[] args) {
 
     }
