46c4e6f9ef0746a87b2f05958e22b96e89017c3c
Sahid Reza <sahidreza976@gmail.com>
2026-08-24 08:00:55 +0530

    Add the function

--- src/main/java/org/example/Detect_Remove_Cycle.java
+++ src/main/java/org/example/Detect_Remove_Cycle.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.util.HashSet;
+
 public class Detect_Remove_Cycle {
     public static void removeLoop(Node head){
 
+        HashSet<Node>set = new HashSet<>();
+
+        // pointer to previous node
+        Node prev = null;
+        while(head != null){
+
+            // if node is not present in the map then insert
+            if(!set.contains(head)){
+
+            }
+        }
     }
     public static void main(String[] args) {
 
