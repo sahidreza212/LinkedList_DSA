cdab80b8da05f4682a02263aaf202e98ba93163a
Sahid Reza <sahidreza976@gmail.com>
2026-08-24 18:41:25 +0530

    Add the full function of intersection function

--- src/main/java/org/example/Intersection_Point.java
+++ src/main/java/org/example/Intersection_Point.java
@@ -3,6 +3,17 @@ package org.example;
 public class Intersection_Point {
     public static Node intersectionPoint(Node head1, Node head2){
 
+        while(head2 != null){
+            Node temp = head1;
+            while(temp != null){
+                if(temp == head2){
+                    return head2;
+                }
+                temp = temp.next;
+            }
+            head2 = head2.next;
+        }
+        return null;
     }
     public static void main(String[] args) {
 
