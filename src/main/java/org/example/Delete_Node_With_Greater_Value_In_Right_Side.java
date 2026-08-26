b6e86ac8a3d39a3b8a7d246d5479b764f26a9d17
Sahid Reza <sahidreza976@gmail.com>
2026-08-26 09:04:43 +0530

    Add the if comdition

--- src/main/java/org/example/Delete_Node_With_Greater_Value_In_Right_Side.java
+++ src/main/java/org/example/Delete_Node_With_Greater_Value_In_Right_Side.java
@@ -4,6 +4,9 @@ public class Delete_Node_With_Greater_Value_In_Right_Side {
 
     public static Node computeNode(Node head){
 
+        if(head == null || head.next == null){
+            return head;
+        }
     }
     public static void main(String[] args) {
 
