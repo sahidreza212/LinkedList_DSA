a4e68b882284ba82b6a3703612df2829a74b3a2a
Sahid Reza <sahidreza976@gmail.com>
2026-08-29 08:31:16 +0530

    Add the populateArbit function

--- src/main/java/org/example/Point_Next_Highest_Value_Using_Arbitary.java
+++ src/main/java/org/example/Point_Next_Highest_Value_Using_Arbitary.java
@@ -16,6 +16,15 @@ class Node1{
 public class Point_Next_Highest_Value_Using_Arbitary {
 
 
+    public static Node1 populateArbit(Node1 head){
+        Node1 curr = head;
+        // Initialize the arbit to the next node
+        while(curr != null){
+            curr.arbit = curr.next;
+            curr = curr.next;
+        }
+        return MergeSort(head);
+    }
     public static void main(String[] args) {
 
         // Create a hardcoded linked list
