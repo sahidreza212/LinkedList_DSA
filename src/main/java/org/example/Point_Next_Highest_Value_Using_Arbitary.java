5c2b56c0e2c23a052f4993a3ee01ced4f7b64228
Sahid Reza <sahidreza976@gmail.com>
2026-08-29 08:22:11 +0530

    Create the Node1 class

--- src/main/java/org/example/Point_Next_Highest_Value_Using_Arbitary.java
+++ src/main/java/org/example/Point_Next_Highest_Value_Using_Arbitary.java
@@ -1,5 +1,17 @@
 package org.example;
 
+class Node1{
+    int data;
+    Node next;
+    // Pointer to next higher node
+    Node arbit;
+
+    Node1(int x) {
+        data = x;
+        next = null;
+        arbit = null;
+    }
+}
 public class Point_Next_Highest_Value_Using_Arbitary {
     public static void main(String[] args) {
 
