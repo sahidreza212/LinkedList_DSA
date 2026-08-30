cd142b7703b77396ec7f854d6f865a3225425769
Sahid Reza <sahidreza976@gmail.com>
2026-08-30 08:26:17 +0530

    Add the Node2

--- src/main/java/org/example/Sort_Biotonic_DLL.java
+++ src/main/java/org/example/Sort_Biotonic_DLL.java
@@ -1,4 +1,15 @@
 package org.example;
 
+class Node2 {
+    int data;
+    Node next;
+    Node prev;
+
+    Node2(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
 public class Sort_Biotonic_DLL {
 }
