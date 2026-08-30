ccd23cd5c1a3a6ecede7304d1be90a276e44ac00
Sahid Reza <sahidreza976@gmail.com>
2026-08-30 07:01:33 +0530

    create a singly linked list

--- src/main/java/org/example/Rearrange_LL_Alternate_First_And_Last.java
+++ src/main/java/org/example/Rearrange_LL_Alternate_First_And_Last.java
@@ -3,5 +3,11 @@ package org.example;
 public class Rearrange_LL_Alternate_First_And_Last {
     public static void main(String[] args) {
 
+        // singly linked list 1->2->3->4->5
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
     }
 }
