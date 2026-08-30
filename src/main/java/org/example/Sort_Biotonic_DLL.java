53052fc2a065eb48a228c1593a52ccecff66d355
Sahid Reza <sahidreza976@gmail.com>
2026-08-30 08:33:24 +0530

    Add the code till now

--- src/main/java/org/example/Sort_Biotonic_DLL.java
+++ src/main/java/org/example/Sort_Biotonic_DLL.java
@@ -2,8 +2,8 @@ package org.example;
 
 class Node2 {
     int data;
-    Node next;
-    Node prev;
+    Node2 next;
+    Node2 prev;
 
     Node2(int val) {
         data = val;
@@ -16,6 +16,20 @@ public class Sort_Biotonic_DLL {
     // function to sort doubly linked list
     public static Node2 sortBiotonicDLL(Node2 head){
 
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        // find the last
+        Node2 last = head;
+        while(last.next != null){
+            last = last.next;
+        }
+
+        // Initialize the pointer
+        Node2 front = head;
+        Node2 result = null;
+        Node2 tail = null;
     }
     public static void main(String[] args) {
 
