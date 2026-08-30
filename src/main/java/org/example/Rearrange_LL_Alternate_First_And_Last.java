6b10699d79c68e11852c9d17d055695c5edd758b
Sahid Reza <sahidreza976@gmail.com>
2026-08-30 07:02:36 +0530

    Add the reverseList method

--- src/main/java/org/example/Rearrange_LL_Alternate_First_And_Last.java
+++ src/main/java/org/example/Rearrange_LL_Alternate_First_And_Last.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Rearrange_LL_Alternate_First_And_Last {
+
+    public static Node reverseList(Node node){
+
+    }
     public static void main(String[] args) {
 
         // singly linked list 1->2->3->4->5
